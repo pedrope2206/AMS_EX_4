a9c19509738bb675371538f208a40660a95a403a
pedrope2206 <pivettapedro93@gmail.com>
2022-05-27 16:35:50 -0300

    AMS_EX_4

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double C = 0;
+double F = 0;
+string nome = "";
+
+
+Console.WriteLine("Olá qual é o seu nome?");
+nome = Console.ReadLine();
+
+Console.WriteLine($"Olá, {nome} quantos graus quer converter?" );
+C = Convert.ToDouble(Console.ReadLine());
+
+F = (9 * C + 160) / 5;
+
+Console.WriteLine($"{nome}, {C} gráus celius valem {F}  fahrenheit");
+
